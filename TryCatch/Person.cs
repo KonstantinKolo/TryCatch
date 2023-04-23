5d1cda03fa3bbaa8e31eb6f3796213952c9199a1
Konstantin <konstantinkolovski_zh21@schoolmath.eu>
2023-04-23 17:16:18 +0300

    Initial commit

---
+++ TryCatch/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Person
+    {
+        private string firstName;
+        private string lastName;
+        private int age;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public string FirstName
+        {
+            get { return this.firstName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException($"{value}\nName cannot be empty or null");
+                }                              
+                this.firstName = value;                
+            }
+        }
+        public string LastName
+        {
+            get { return this.lastName; }
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentNullException($"{value}\nLast name cannot be empty or null");
+                }
+                this.lastName = value;
+            }
+        }
+        public int Age
+        {
+            get { return this.age; }
+            set
+            {
+                if (value > 120 || value < 0)
+                {
+                    throw new ArgumentNullException($"{value}\nAge must be between 0 and 120");
+                }
+                this.age = value;
+            }
+        }
+    }
+}
---
+++ TryCatch/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Person noName = new Person(string.Empty, "Kolovski", 15);
+            }
+            catch(ArgumentNullException ex)
+            {
+                Console.WriteLine("Exception thrown : {0}",ex);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Exception thrown : {0}", ex);
+            }
+
+            try
+            {
+                Person noLastName = new Person("Konstantin", null, 15);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Exception thrown : {0}", ex);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Exception thrown : {0}", ex);
+            }
+
+            try
+            {
+                Person negativeAge = new Person("Konstantin", "Kolovski", -15);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Exception thrown : {0}", ex);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Exception thrown : {0}", ex);
+            }
+
+            try
+            {
+                Person tooOld = new Person("Konstantin", "Kolovski", 165);
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Exception thrown : {0}", ex);
+            }
+            catch(ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("Exception thrown : {0}",ex);
+            }
+        }
+    }
+}
